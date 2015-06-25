79427031061912d42741341c718d8b33fc62b102
root <twistedogic@gmail.com>
2015-06-25 04:15:30 +0000

    update input and output data structure

--- test/marathon-test.js
+++ test/marathon-test.js
@@ -1,9 +1,11 @@
 var marathon = require('../lib/marathon.js');
 describe("Get marathon task list",function(){
     it("list is good",function(done){
-        marathon('10.0.0.129',function(err,res){
+        marathon({host:'10.0.0.129', dockeport:4243},function(err,res){
             if (res.length == 3){
-                done();
+                if(res[0].ip){
+                    done();
+                }
             }
         })
     })
